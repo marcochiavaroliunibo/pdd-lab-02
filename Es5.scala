d2f7fbf9ca729c185d0cb55fe4fd6536705e26d4
MarcoChiavaroli99 <marcochiava.mc@gmail.com>
2022-03-10 13:39:36 +0100

    fix exercise

--- Es5.scala
+++
@@ -1,9 +0,0 @@
-package u02
-
-object Es5 extends App:
-
-  /* Point 5 - Compose */
-  def compose (f: Int => Int, g: Int => Int)(n: Int) : Int = f(g(n))
-
-  /* Compose generic version */
-  def composeGenerics[A] (f: A => A, g: A => A)(n: A) : A = f(g(n))
